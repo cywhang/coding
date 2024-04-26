d1553a1c85af2eb55537a55daa30a86bf3866d63
cywha <cywhang2@naver.com>
2024-04-27 08:21:57 +0900

    0427 - 99%

---
+++ src/number99.java
@@ -0,0 +1,16 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class number99 {
+    public static void main(String[] args) {
+        int n = 4;
+        int[][] answer = solution(n);
+        System.out.println("answer: " + Arrays.deepToString(answer));
+    }
+
+    public static int[][] solution(int n) {
+        int[][] answer = {};
+
+        return answer;
+    }
+}
--- src/picture.java
+++ src/picture.java
@@ -10,11 +10,77 @@ public class picture {
     /*
         picture는 1:1 비율의 그림이고
         k배 만큼 picture를 확대해야함.
+
+            .xx...xx.
+            x..x.x..x                   x.x
+            x...x...x                   .x.
+            .x.....x.                   x.x
+            ..x...x..
+            ...x.x...
+            ....x....
+
+                 |                      |
+                2배                     3배
+                 |                      |
+
+        ..xxxx......xxxx..          xxx...xxx
+        ..xxxx......xxxx..          xxx...xxx
+        xx....xx..xx....xx          xxx...xxx
+        xx....xx..xx....xx          ...xxx...
+        xx......xx......xx          ...xxx...
+        xx......xx......xx          ...xxx...
+        ..xx..........xx..          xxx...xxx
+        ..xx..........xx..          xxx...xxx
+        ....xx......xx....          xxx...xxx
+        ....xx......xx....
+        ......xx..xx......
+        ......xx..xx......
+        ........xx........
+        ........xx........
      */
     public static String[] solution(String[] picture, int k) {
-        String[] answer = {};
+        int rows = picture.length;
+        int cols = picture[0].length();
 
+        int newRows = rows * k;
+        int newCols = cols * k;
+
+        String[] newPicture = new String[newRows];
+
+        for (int i = 0; i < newRows; i++) {
+            StringBuilder sb = new StringBuilder();
+            int row = i / k;
+
+            for (int j = 0; j < newCols; j++) {
+                int col = j / k;
+                char pixel = picture[row].charAt(col);
+                sb.append(pixel);
+            }
+
+            newPicture[i] = sb.toString();
+        }
+
+        return newPicture;
+    }
+}
+
+/*
+    public String[] solution(String[] picture, int k) {
+        String[] answer = new String[picture.length * k];
+        int idx = 0;
+
+        for(int i=0; i<picture.length; i++) {
+            for(int j=0; j<k; j++) {
+                StringBuilder sb = new StringBuilder();
+
+                for(int l=0; l<picture[i].length(); l++) {
+                    sb.append(String.valueOf(picture[i].charAt(l)).repeat(k));
+                }
+
+                answer[idx++] = sb.toString();
+            }
+        }
 
         return answer;
     }
-}
+ */
